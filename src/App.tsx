0c72561138ad95b65843674aaf3247b706287506
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-04-04 14:52:37 +0900

    ログイン機能の追加など

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "@/pages/Signin";
 import Layout from "./Layout";
+import Signup from "@/pages/Signup";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/" element={<Layout />} />
-        {/* 他のルート */}
       </Routes>
     </Router>
   );
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
-import { Link, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { authRepository } from "@/modules/auth/auth.repository";
 import { useCurrentUserStore } from "@/modules/auth/current-user.state";
 
-const Signin = () => {
+const Signup = () => {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const currentUserStore = useCurrentUserStore();
 
-  const signin = async () => {
-    const user = await authRepository.signin(email, password);
+  const signup = async () => {
+    const user = await authRepository.signup(name, email, password);
     currentUserStore.set(user);
-  };
+  }
 
   if (currentUserStore.currentUser != null) return <Navigate replace to="/" />;
 
@@ -22,10 +23,15 @@ const Signin = () => {
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <Card className="w-full max-w-md p-6 shadow-lg">
         <CardHeader>
-          <CardTitle className="text-center text-xl">顧客管理デモアプリ</CardTitle>
+          <CardTitle className="text-center text-xl">ユーザー登録</CardTitle>
         </CardHeader>
         <CardContent>
           <div className="space-y-4">
+            <Input
+              placeholder="名前"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
             <Input
               placeholder="メールアドレス"
               value={email}
@@ -37,21 +43,14 @@ const Signin = () => {
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
-            <Button className="w-full" onClick={ signin } disabled={email === '' || password === ''}>
-              ログイン
+            <Button className="w-full" onClick={ signup } disabled={name === "" || email === '' || password === ''}>
+              登録
             </Button>
-          </div>
-          <div className="mt-4 text-center text-sm">
-            登録は
-            <Link className="underline" to={'/signup'}>
-              こちら
-            </Link>
-            から
           </div>
         </CardContent>
       </Card>
     </div>
   );
-};
+}
 
-export default Signin;
+export default Signup;
