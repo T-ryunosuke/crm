0faf0670f65bf8995f054d99e5afdbca7b189a4e
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-04-05 17:46:35 +0900

    ログイン状態の保持

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,26 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signin from "@/pages/Signin";
 import Layout from "./Layout";
 import Signup from "@/pages/Signup";
+import { useEffect, useState } from "react";
+import { useCurrentUserStore } from "./modules/auth/current-user.state";
+import { authRepository } from "./modules/auth/auth.repository";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const currentUserStore = useCurrentUserStore();
+
+  useEffect(() => {
+    setSession();
+  }, [])
+
+  const setSession = async () => {
+    const currentUser = await authRepository.getCurrentUser();
+    currentUserStore.set(currentUser);
+    setIsLoading(false);
+  }
+
+  if (isLoading) return <div />;
+
   return (
     <Router>
       <Routes>
--- src/modules/auth/current-user.state.ts
+++ src/modules/auth/current-user.state.ts
@@ -1,9 +1,21 @@
 import { User } from "@supabase/supabase-js";
 import { atom, useAtom } from "jotai";
 
+// jotaiはatomに値を入れて管理している
+// 型はsupabaseのUser型
 const currentUserAtom = atom<User>();
 
 export const useCurrentUserStore = () => {
   const [currentUser, setCurrentUser] = useAtom(currentUserAtom);
   return { currentUser, set: setCurrentUser };
 };
+
+// 使い方
+// まず宣言
+// const currentUserStore = useCurrentUserStore();
+
+// setでsetCurrentUserにユーザーのデータを渡すことでatomが更新される
+// currentUserStore.set(userData);
+
+// atomの中のユーザー情報を参照する場合
+// currentUserStore.currentUser;
