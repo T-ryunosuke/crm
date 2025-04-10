340066a41a36b6ef064193f7f88375211254e2b9
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-04-10 23:14:26 +0900

    今いるページのメニューをハイライト

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,32 +1,36 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import UserItem from "./UserItem";
 import { useCurrentUserStore } from "@/modules/auth/current-user.state";
 import { authRepository } from "@/modules/auth/auth.repository";
 
 const Sidebar = () => {
   const currentUserStore = useCurrentUserStore();
+  // 現在のパスを取得
+  const location = useLocation();
 
-const signout = async () => {
-  try {
-    await authRepository.signout();
-  } catch (err) {
-    console.error("ログアウトエラー", err);
-  } finally {
-    currentUserStore.set(undefined);
-  }
-};
+  const signout = async () => {
+    try {
+      await authRepository.signout();
+    } catch (err) {
+      console.error("ログアウトエラー", err);
+    } finally {
+      currentUserStore.set(undefined);
+    }
+  };
+
+  const isActive = (path: string) => location.pathname === path;
 
   return (
     <div className="hidden sm:block w-56 bg-gray-800 text-white h-screen fixed top-14 pt-4">
-      <UserItem
-        user={currentUserStore.currentUser!}
-      />
+      <UserItem user={currentUserStore.currentUser!} />
       <div className="pl-2 pb-2 text-sm">メニュー</div>
       <ul>
         <li>
           <Link
             to="/"
-            className="block px-5 py-3 font-mono text-lg tracking-widest hover:bg-gray-700"
+            className={`block px-5 py-3 font-mono text-lg tracking-widest hover:bg-gray-700 ${
+              isActive("/") ? "bg-gray-700" : ""
+            }`}
           >
             顧客一覧
           </Link>
@@ -34,7 +38,9 @@ const signout = async () => {
         <li>
           <Link
             to="/create"
-            className="block px-5 py-3 font-mono text-lg tracking-widest hover:bg-gray-700"
+            className={`block px-5 py-3 font-mono text-lg tracking-widest hover:bg-gray-700 ${
+              isActive("/create") ? "bg-gray-700" : ""
+            }`}
           >
             顧客登録
           </Link>
