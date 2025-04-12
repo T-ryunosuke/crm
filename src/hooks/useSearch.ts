316af353afa460a258504f96d1ff58bc636cd8b4
Ryunosuke <dog.kira@yahoo.ne.jp>
2025-04-12 18:39:32 +0900

    会社名でも検索可能に変更

--- src/hooks/useSearch.ts
+++ src/hooks/useSearch.ts
@@ -1,13 +1,18 @@
 import { useState } from "react";
 import { CustomerTypes } from "@/types/customer";
 
-export const useSearch = (data: CustomerTypes[], queryKey: "name" | "company") => {
+export const useSearch = (data: CustomerTypes[]) => {
   const [searchQuery, setSearchQuery] = useState<string>("");
 
   // 検索結果を取得
-  const filteredData = data.filter((item) =>
-    item[queryKey].includes(searchQuery)
-  );
+  // dataという配列から一つずつオブジェクト(customer)を取り出して、customerのnameかcompanyにsearchQuery（入力された情報）が含まれているかをチェックして、その条件を満たす要素だけを抜き出して、新しい配列（filteredData）を作っている。
+  const filteredData = data.filter((customer) => {
+    const query = searchQuery.toLowerCase();
+    return (
+      customer.name.toLowerCase().includes(query) ||
+      customer.company.toLowerCase().includes(query)
+    );
+  });
 
   return { searchQuery, setSearchQuery, filteredData };
 };
